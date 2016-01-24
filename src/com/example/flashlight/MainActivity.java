0e2f6c3ec31f16d80cd5712b12d816b20b55dd34
zhuwentao <215069587@qq.com>
2016-01-24 13:41:38 +0800

    zhuwentao

---
+++ src/com/example/flashlight/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.flashlight;
+
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	boolean isFlash;	// 控制器，为true时开，false关
+	Camera camera;
+	Button mButtonOpenOrClose;	// 控制开关
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mButtonOpenOrClose = (Button) findViewById(R.id.open_flashlight);
+		camera = Camera.open();
+		isFlash = true;
+	}
+	/**
+	 * 操作手电筒
+	 * @param v
+	 */
+	public void openOrClose(View v){
+		if(isFlash){
+			Parameters mParameters = camera.getParameters();
+			// 更改手电筒的状态
+			mParameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+			camera.setParameters(mParameters);
+			mButtonOpenOrClose.setText("关闭手电筒");
+			isFlash = false;
+		}else{
+			Parameters mParameterss = camera.getParameters();
+			mParameterss.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+			camera.setParameters(mParameterss);
+			mButtonOpenOrClose.setText("打开手电筒");
+			isFlash = true;
+		}	
+	}
+	
+	// 应用退出就关闭
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		
+		camera.release();	// 释放手电筒
+	}
+	
+
+	
+
+}
